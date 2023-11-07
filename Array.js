8344bcd8dd7a34af5334c43faeaeca642bf36a1d
Aderibigbe133 <github947@gmail.com>
2023-11-07 12:26:30 +0100

    Presentation

---
+++ Array.js
@@ -0,0 +1,55 @@
+// at ()
+
+//  const fruits = ["Banana", "Orange", "Apple", "Mango"];
+//  let index = 2;
+//  let fruit = fruits.at(index);
+//  console.log(fruit)
+
+// // Concat
+//  let number = [0, 1, 2, 3, 4]
+//  console.log(number.concat([5, 6, 7, 8, 9]))
+
+
+// copyWithin()
+// const fruit = ['banana', 'orange', 'Apple', 'Mango']
+// const result = fruit.copyWithin(2, 0);
+// console.log(result)
+
+
+ // entries method
+
+// const array1 = ['a', 'b', 'c'];
+
+// const iterator1 = array1.entries();
+
+// console.log(iterator1.next().value);
+// // Expected output: Array [0, "a"]
+
+// console.log(iterator1.next().value);
+// // Expected output: Array [1, "b"]
+
+
+// // Every
+//  const numb = [9, 2, 3, 4, 5, 6]
+//  let above = numb.every(uy)
+
+// function uy (age){
+// return age > 1
+// }
+// console.log(above)
+
+
+// Fill()
+// const fruits = ["Banana", "Orange", "Apple", "Mango"];
+// const result = fruits.fill("Kiwi", 1, 3);
+// console.log(result)
+
+
+// // Filter
+// const ages = [12, 33, 16, 40, 10, 25, 12];
+// const result = ages.filter(checkAdult);
+
+// function checkAdult(age) {
+//   return age >= 18 && age <= 40
+// }
+// console.log(result)
